8c13ec11eb0d27c83993fde598a426da6530cafb
JasmineGonzalez10 <111079031+JasmineGonzalez10@users.noreply.github.com>
2023-08-14 15:56:18 -0700

    Update generate_from_checkpoint.py

--- anticipation/generate_from_checkpoint.py
+++ anticipation/generate_from_checkpoint.py
@@ -3,7 +3,7 @@ import numpy as np
 import torch
 
 import sys
-sys.path.insert(0, '/john1/scr1/gonzalez/anticipation')
+sys.path.insert(0, '/jagupard26/scr1/gonzalez2/anticipation')
 
 from transformers import GPT2LMHeadModel
 
@@ -22,7 +22,7 @@ model_name = 'absurd-deluge-4'
 model = GPT2LMHeadModel.from_pretrained(f'/nlp/scr/jthickstun/absurd-deluge-4/step-100000/hf').cuda()
 
 prompt = []
-controls = midi_to_events(open("/john1/scr1/gonzalez/twinkle.mid", "r"))
+controls = midi_to_events(open("/jagupard26/scr1/gonzalez2/twinkle.mid", "r"))
 
 #PAD WITH INSTRUMENT CONTROLS
 instruments = [65, 0, 128, 26, 33, 55026, 55026, 55026, 55026, 55026, 55026, 55026, 55026, 55026, 55026, 55026]
@@ -31,4 +31,4 @@ controls = controls + instruments
 generated_tokens = generate(model, 0, LENGTH_IN_SECONDS, prompt, controls, top_p=0.98, debug=True)
 print(controls)
 mid = events_to_midi(ops.clip(generated_tokens, 0, LENGTH_IN_SECONDS))
-mid.save(f'/john1/scr1/gonzalez/model_output/{model_name}/generated-twinkle.mid')
+mid.save(f'/jagupard26/scr1/gonzalez2/model_output/{model_name}/generated-twinkle.mid')
