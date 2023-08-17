ad6acdbff91f379362664259ecb8de1e6798f346
JasmineGonzalez10 <111079031+JasmineGonzalez10@users.noreply.github.com>
2023-08-17 14:25:46 -0700

    Update generate_from_checkpoint.py

--- anticipation/generate_from_checkpoint.py
+++ anticipation/generate_from_checkpoint.py
@@ -22,20 +22,13 @@ model_name = 'absurd-deluge-4'
 model = GPT2LMHeadModel.from_pretrained(f'/nlp/scr/jthickstun/absurd-deluge-4/step-100000/hf').cuda()
 
 prompt = []
-controls = midi_to_events(f'/jagupard26/scr1/gonzalez2/model_input/lion.mid')
-#compound = events_to_compound(controls)
-# moving all notes to be in octave C4-C5
-#for i in range(len(compound[2::5])):
-    #compound[i*5 + 2] = (compound[i*5 + 2] + 12)
-#controls = compound_to_events(compound)
+controls = midi_to_events(f'/jagupard26/scr1/gonzalez2/model_input/twinkle_final.mid')
 controls = [tok + CONTROL_OFFSET for tok in controls]
 
 #PAD WITH INSTRUMENT CONTROLS
 instruments = [65, 0, 73, 25, 128, 55026, 55026, 55026, 55026, 55026, 55026, 55026, 55026, 55026, 55026, 55026]
 controls = instruments + controls
-print(controls)
 
-generated_tokens = generate(model, 0, LENGTH_IN_SECONDS, prompt, controls, top_p=0.98, debug=True)
-print("Generated tokens: ", generated_tokens)
+generated_tokens = generate(model, 0, LENGTH_IN_SECONDS, prompt, controls, top_p=0.98, debug=False)
 mid = events_to_midi(generated_tokens)
-mid.save(f'/jagupard26/scr1/gonzalez2/model_output/{model_name}/generated-lion.mid')
+mid.save(f'/jagupard26/scr1/gonzalez2/model_output/{model_name}/generated-twinkle.mid')
