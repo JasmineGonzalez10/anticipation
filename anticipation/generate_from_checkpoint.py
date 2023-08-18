02a35993902dffbe73f6a99ae04100288aad21f8
JasmineGonzalez10 <111079031+JasmineGonzalez10@users.noreply.github.com>
2023-08-18 13:40:00 -0700

    Update generate_from_checkpoint.py

--- anticipation/generate_from_checkpoint.py
+++ anticipation/generate_from_checkpoint.py
@@ -14,19 +14,22 @@ from anticipation.convert import *
 
 from anticipation.vocab import SEPARATOR
 
-LENGTH_IN_SECONDS = 15
+LENGTH_IN_SECONDS = 20
 
 model_name = 'absurd-deluge-4'
 #step_number = 10000
 
 model = GPT2LMHeadModel.from_pretrained(f'/nlp/scr/jthickstun/absurd-deluge-4/step-100000/hf').cuda()
 
-prompt = []
+prompt = [SEPARATOR, SEPARATOR, SEPARATOR]
+#prompt =[]
 controls = midi_to_events(f'/jagupard26/scr1/gonzalez2/model_input/twinkle_final.mid')
 controls = [tok + CONTROL_OFFSET for tok in controls]
+# adding delay for sampling
+#controls[0::3] = [tok + 500 for tok in controls[0::3]]
 
 #PAD WITH INSTRUMENT CONTROLS
-instruments = [65, 0, 73, 25, 128, 55026, 55026, 55026, 55026, 55026, 55026, 55026, 55026, 55026, 55026, 55026]
+instruments = [24, 0, 73, 128, 55026, 55026, 55026, 55026, 55026, 55026, 55026, 55026, 55026, 55026, 55026, 55026]
 controls = instruments + controls
 
 generated_tokens = generate(model, 0, LENGTH_IN_SECONDS, prompt, controls, top_p=0.98, debug=False)
