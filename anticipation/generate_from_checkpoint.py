c4c6fb3b6dc663c70ef6c8d510a1edafc7e50f03
JasmineGonzalez10 <111079031+JasmineGonzalez10@users.noreply.github.com>
2023-08-21 16:12:00 -0700

    Update generate_from_checkpoint.py

--- anticipation/generate_from_checkpoint.py
+++ anticipation/generate_from_checkpoint.py
@@ -14,7 +14,7 @@ from anticipation.convert import *
 
 from anticipation.vocab import SEPARATOR
 
-LENGTH_IN_SECONDS = 20
+LENGTH_IN_SECONDS = 15
 
 model_name = 'absurd-deluge-4'
 #step_number = 10000
@@ -22,16 +22,26 @@ model_name = 'absurd-deluge-4'
 model = GPT2LMHeadModel.from_pretrained(f'/nlp/scr/jthickstun/absurd-deluge-4/step-100000/hf').cuda()
 
 prompt = [SEPARATOR, SEPARATOR, SEPARATOR]
-#prompt =[]
+#prompt = []
 controls = midi_to_events(f'/jagupard26/scr1/gonzalez2/model_input/twinkle_final.mid')
+
+###########
+'''
+compound = events_to_compound(controls)
+for i in range(len(compound[2::5])):
+    compound[i*5 + 2] = compound[i*5 + 2] - 4
+controls = compound_to_events(compound)
+''' ###########
+
 controls = [tok + CONTROL_OFFSET for tok in controls]
 # adding delay for sampling
 #controls[0::3] = [tok + 500 for tok in controls[0::3]]
 
 #PAD WITH INSTRUMENT CONTROLS
-instruments = [24, 0, 73, 128, 55026, 55026, 55026, 55026, 55026, 55026, 55026, 55026, 55026, 55026, 55026, 55026]
+instruments = [66, 0, 128, 32, 55026, 55026, 55026, 55026, 55026, 55026, 55026, 55026, 55026, 55026, 55026, 55026]
 controls = instruments + controls
 
-generated_tokens = generate(model, 0, LENGTH_IN_SECONDS, prompt, controls, top_p=0.98, debug=False)
-mid = events_to_midi(generated_tokens)
-mid.save(f'/jagupard26/scr1/gonzalez2/model_output/{model_name}/generated-twinkle.mid')
+for i in range(5):
+    generated_tokens = generate(model, 0, LENGTH_IN_SECONDS, prompt, controls, top_p=0.98, debug=True)
+    mid = events_to_midi(generated_tokens)
+    mid.save(f'/jagupard26/scr1/gonzalez2/model_output/{model_name}/generated-twinkle-818-{i}.mid')
